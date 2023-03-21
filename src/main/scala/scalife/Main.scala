4a98af98884794da381562e8c5824372e058227d
ctare <sankaku9006221@gmail.com>
2023-03-21 18:03:52 +0900

    Set app icon/title

--- src/main/scala/scalife/Main.scala
+++ src/main/scala/scalife/Main.scala
@@ -2,21 +2,23 @@ package scalife
 
 import processing.core.PApplet
 
-object Main {
+object Main:
   def main(args: Array[String]): Unit = {
     PApplet.main("scalife.Main")
   }
-}
 
 class Main extends PApplet:
   override def settings(): Unit = {
     size(700, 700)
   }
 
-//  override def setup(): Unit = {
-//
-//  }
+  override def setup(): Unit = {
+    surface.setIcon(loadImage("logo.png"))
+    surface.setTitle("Scalife")
+  }
 
   override def draw(): Unit = {
-
+    background(0)
+    ellipse(mouseX, mouseY, 50, 50)
+    fill(255)
   }
