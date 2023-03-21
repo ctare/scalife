76ed98d5e35a95f9741f231ab8b48908c1d512ee
ctare <sankaku900622@yahoo.co.jp>
2023-03-21 16:34:25 +0900

    create project

---
+++ src/main/scala/scalife/Main.scala
@@ -0,0 +1,22 @@
+package scalife
+
+import processing.core.PApplet
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    PApplet.main("scalife.Maina")
+  }
+}
+
+class Maina extends PApplet:
+  override def settings(): Unit = {
+    size(700, 700)
+  }
+
+//  override def setup(): Unit = {
+//
+//  }
+
+  override def draw(): Unit = {
+
+  }
---
+++ src/test/scala/scalife/Main.scala
@@ -0,0 +1,7 @@
+package scalife
+
+import org.scalatest.*
+import org.scalatest.funsuite.AnyFunSuite
+
+class MainSpec extends AnyFunSuite {
+}
